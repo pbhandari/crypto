63a88b719e21b072cb75b60ca5443f6614c6c647
Adam Osvaldo <adam.ortiz@mail.utoronto.ca>
2014-01-11 01:10:47 -0500

    wrote stubs for finding the keys and added decryption algorithm
    the decryption is just the inverse of the encryption method

--- JavaScript/Caeser.js
+++ JavaScript/Caeser.js
@@ -11,6 +11,9 @@ var alpha = new Array("a", "b", "c", "d", "e", "f", "g",
 
 function encrypt(text, shift, alphabet)
 {
+    /*
+    *basic encryption algorithm
+    */
 
     //for other alphabets
     var alphabet = typeof alphabet !== 'undefined' ? alphabet : alpha; 
@@ -38,3 +41,58 @@ function encrypt(text, shift, alphabet)
     return ciphertext;
 
 }
+
+
+function decrypt(text, shift, alphabet)
+{
+    /*
+    *basic decryption algorithm
+    */
+
+    //for other alphabets
+    var alphabet = typeof alphabet !== 'undefined' ? alphabet : alpha; 
+    var text_lower = text.toLowerCase();
+
+    //holds the ciphertext
+    var ciphertext = "";
+
+    for(var i=0; i<text_lower.length; i++)
+    {
+
+	//place the current letter is in the alphabet
+	var plaintext_index = alphabet.indexOf(text_lower[i]);
+
+	if (plaintext_index != -1)
+        {
+	    //place the index is shifted 
+	    var ciphertext_index = (plaintext_index - shift)%alphabet.length;
+	
+            //retrieve the new letter
+            ciphertext = ciphertext + alphabet[ciphertext_index];
+        }
+    }
+
+    return ciphertext;
+}
+
+function ciphertext()
+{
+    /*Only a copy of the cipher text is known*/
+}
+
+function known_plaintext()
+{
+    /*a copy of both the plaintext and ciphertext is known*/
+}
+
+function chosen_plaintext()
+{
+   /*access to encryption*/
+}
+
+function chosen_ciphertext()
+{
+   /*access to decryption machine*/
+}
+
+
