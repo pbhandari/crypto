ccf97029c248c396acd74d04a0ae63537ddf3872
Adam Osvaldo <adam.ortiz@mail.utoronto.ca>
2014-03-02 14:06:30 -0500

    added comments to caesar.rb

--- Ruby/Caesar.rb
+++ Ruby/Caesar.rb
@@ -1,15 +1,32 @@
-
+=begin
+*******************************
+ * By Adam Ortiz
+ * implementation of Caeser/shift 
+ * cipher in PHP.
+*******************************
+=end
 def mod(n, m)
     return ((n % m) + m) % m
 end
 
 class Caesar
 
+=begin
+    * creates a new Caeser, in order to encrypt and 
+    * decrypt from
+    * shiftval - shift value number
+    * alphabetVal - alphabet array to be used 
+=end
     def initialize(shiftVal, alphaVal=('a'..'z').to_a)
         @shift = shiftVal
         @alphabet = alphaVal
     end
     
+=begin
+    * Used to encrypt plaintext,
+    * param plaintext
+    * returns ciphertext 
+=end
     def encrypt(plaintext)
         ciphertext = ""
         for i in 0...plaintext.length
@@ -20,6 +37,11 @@ class Caesar
         return ciphertext
     end
 
+=begin
+    * Used to decrypt ciphertext,
+    * param ciphertext 
+    * returns plaintext
+=end
     def decrypt(ciphertext)
         plaintext = ""
         for i in 0...ciphertext.length
@@ -31,12 +53,20 @@ class Caesar
     end
 end
 
+=begin
+* class which holds the known exploits for caesar cipher
+=end
 class Exploits
 
     def initialize(alphaVal=('a'..'z').to_a)
         @alphabet = alphaVal
     end
 
+=begin
+    * Only a copy of the cipher text is known, the best
+    * course of action in this case is to try all 
+    * possibilities.
+=end
     def ciphertext_only(ciphertext)
         for i in 0...@alphabet.length
             c = Caesar.new(i)
@@ -44,6 +74,10 @@ class Exploits
         end 
     end
     
+=begin
+    * a copy of both the plaintext and ciphertext is known,
+    * deduce the key
+=end
     def known_plaintext(plaintext, ciphertext)
         i = @alphabet.index(plaintext[0])
         j = @alphabet.index(ciphertext[0])
@@ -51,11 +85,19 @@ class Exploits
         return key
     end
 
+=begin
+    * access to encryption machine. currently passes a
+    * an object instance.
+=end
     def chosen_plaintext(cipher)
         letter = cipher.encrypt(@alphabet[0])
         return @alphabet.index(letter)
     end
     
+=begin
+    * access to decryption machine. currently passes a
+    * an object instance.
+=end
     def chosen_ciphertext(cipher)
         letter = cipher.decrypt(@alphabet[0])
         index =  @alphabet.index(letter)
