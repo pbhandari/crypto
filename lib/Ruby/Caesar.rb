f3c5a3068c047ecc144a64111e1fea5835a9ab44
Adam Osvaldo <adam.ortiz@mail.utoronto.ca>
2014-03-10 21:21:50 -0400

    added the ruby alphabet and implemented Caesar using it, needs to be re-worked to use native methods

--- lib/Ruby/Caesar.rb
+++ lib/Ruby/Caesar.rb
@@ -1,3 +1,4 @@
+require '../../etc/Alphabet/Ruby/Alphabet.rb'
 =begin
 *******************************
  * By Adam Ortiz
@@ -17,9 +18,13 @@ class Caesar
     * shiftval - shift value number
     * alphabetVal - alphabet array to be used 
 =end
-    def initialize(shiftVal, alphaVal=('a'..'z').to_a)
+    def initialize(shiftVal, alphaVal="../../etc/Frequency/English.csv")
         @shift = shiftVal
-        @alphabet = alphaVal
+        if alphaVal.is_a?(Alphabet)
+            @alphabet = alphaVal
+        else
+            @alphabet = Alphabet.new(alphaVal)
+        end
     end
     
 =begin
@@ -32,7 +37,7 @@ class Caesar
         for i in 0...plaintext.length
             key = @alphabet.index(plaintext[i]);
             key = mod(key+@shift, @alphabet.length)
-            ciphertext = ciphertext + @alphabet[key]
+            ciphertext = ciphertext + @alphabet.at(key)[0]
         end
         return ciphertext
     end
@@ -47,7 +52,7 @@ class Caesar
         for i in 0...ciphertext.length
             key = @alphabet.index(ciphertext[i]);
             key = mod(key-@shift, @alphabet.length)
-            plaintext = plaintext + @alphabet[key]
+            plaintext = plaintext + @alphabet.at(key)[0]
         end
         return plaintext
     end
@@ -58,8 +63,8 @@ end
 =end
 class Exploits
 
-    def initialize(alphaVal=('a'..'z').to_a)
-        @alphabet = alphaVal
+    def initialize(alphaVal="../../etc/Frequency/English.csv")
+        @alphabet = Alphabet.new(alphaVal)
     end
 
 =begin
@@ -69,7 +74,7 @@ class Exploits
 =end
     def ciphertext_only(ciphertext)
         for i in 0...@alphabet.length
-            c = Caesar.new(i)
+            c = Caesar.new(i, @alphabet)
             print c.decrypt(ciphertext) + "\n"
         end 
     end
@@ -90,7 +95,7 @@ class Exploits
     * an object instance.
 =end
     def chosen_plaintext(cipher)
-        letter = cipher.encrypt(@alphabet[0])
+        letter = cipher.encrypt(@alphabet.at(0)[0])
         return @alphabet.index(letter)
     end
     
@@ -99,7 +104,7 @@ class Exploits
     * an object instance.
 =end
     def chosen_ciphertext(cipher)
-        letter = cipher.decrypt(@alphabet[0])
+        letter = cipher.decrypt(@alphabet.at(0)[0])
         index =  @alphabet.index(letter)
         index = mod(index * (-1), @alphabet.length)
         return index
