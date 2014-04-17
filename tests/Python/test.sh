65b7a80c5e410b871a48137e0ff1a67317061730
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-04-16 21:22:38 -0400

    Fix Python/test.sh script to work with new style test

--- tests/Python/test.sh
+++ tests/Python/test.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
+
 cd `dirname $0`
-echo '**************Test Caesar*****************'
-PYTHONPATH=../../lib/Python/:../../etc/Alphabet/Python/ python2.7 CaesarTest.py
-echo '**************Test Caesar*****************'
+
+tests=""
+
+opts=$(getopt -o"hf:" -l"help,func:" -n${0##*/} -- "$@");
+eval set -- "${opts%--}"
+
+until [[ -z "$1" ]]; do
+    case $1 in
+        -f|--func)
+            TEST_FUNC=$2
+            shift
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-f FUNCTIONS]"
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+for te in *.py; do
+    if echo "$TEST_FUNC" | grep -i "${te/Test.py}" > /dev/null ; then
+        tests+="$te "
+    fi
+done
+
+for t in ${tests:-*.py}; do
+    echo "**************Test ${t/Test.py}*****************"
+    PYTHONPATH=../../lib/Python/:../../etc/Alphabet/Python/ python2.7 $t
+    echo "**************Test ${t/Test.py}*****************"
+    echo
+done
