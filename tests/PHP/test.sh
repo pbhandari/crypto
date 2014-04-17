32c5c790cb40932dbfb0af6bdb41ad1f8eb3273d
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-04-16 21:22:38 -0400

    Fix PHP/test.sh script to work with new style test

--- tests/PHP/test.sh
+++ tests/PHP/test.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
-cd `dirname $0`
-echo '**************Test Caesar*****************'
-php ./bin/phpunit.phar CaesarTest.php
-echo '**************Test Caesar*****************'
 
+cd `dirname $0`
+
+tests=""
+
+opts=$(getopt -o"hf:" -l"help,func:" -n${0##*/} -- "$@");
+eval set -- "${opts%--}"
+
+until [[ -z "$1" ]]; do
+    case $1 in
+        -f|--func)
+            TEST_FUNC=$2
+            shift
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-f FUNCTIONS]"
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+for te in *.php; do
+    if echo "$TEST_FUNC" | grep -i "${te/Test.php}" > /dev/null ; then
+        tests+="$te "
+    fi
+done
+
+for t in ${tests:-*.php}; do
+    echo "**************Test ${t/Test.php}*****************"
+    php ./bin/phpunit.phar $t
+    echo "**************Test ${t/Test.php}*****************"
+    echo
+done
