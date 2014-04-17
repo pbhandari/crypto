eaf61d510912a36c3f47511951776a31a84f8a3e
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-04-16 21:22:38 -0400

    Fix Ruby/test.sh script to work with new style test

--- tests/Ruby/test.sh
+++ tests/Ruby/test.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
+
 cd `dirname $0`
-echo '**************Test Caesar*****************'
-ruby ./CaesarTest.rb
-echo '**************Test Caesar*****************'
+
+tests=""
+
+opts=$(getopt -o"hf:" -l"help,func:" -n${0##*/} -- "$@");
+eval set -- "${opts%--}"
+
+until [[ -z "$1" ]]; do
+    case $1 in
+        -f|--func)
+            TEST_FUNC=$2
+            shift
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-f FUNCTIONS]"
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+for te in *.rb; do
+    if echo "$TEST_FUNC" | grep -i "${te/Test.rb}" > /dev/null ; then
+        tests+="$te "
+    fi
+done
+
+for t in ${tests:-*.rb}; do
+    echo "**************Test ${t/Test.rb}*****************"
+    ruby $t
+    echo "**************Test ${t/Test.rb}*****************"
+    echo
+done
